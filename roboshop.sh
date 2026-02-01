c003e319dcfaa595411b72b7324a185752cce4c2
Premnath <premnathb77@gmail.com>
2026-02-01 21:03:49 +0530

    Modified

--- roboshop.sh
+++ roboshop.sh
@@ -13,16 +13,14 @@ instance_id=$(aws ec2 run-instances \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
-    --output text 
-    )
+    --output text)
 
     if [ $instance == "frontend" ]; then
 
         IP=$(aws ec2 describe-instances \
         --instance-ids $instance_id \
         --query 'Reservations[*].Instances[*].PublicIpAddress' \
-        --output text
-    )
+        --output text)
 
     else
 
